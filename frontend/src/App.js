a2505c0e71dda91dd0d67d5ca0fb56a8bf36a42e
JuanSosaDias <juan.sosadias@correo.ucu.edu.uy>
2025-06-21 12:29:47 -0300

    Creación de Login
    Login está ppronto, si se logea con serie y numero correcto, navega a VotacionPage que est;a haciendo Santi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/admin/loginPage/loginPage'; 
 import InicioPage from './pages/admin/inicioPage/inicioPage';
 import VotacionPage from './pages/admin/votacionPage/votacionPage';
+import VotanteLoginPage from './pages/Votante/loginPage/VotanteLoginPage';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/admin/login" element={<LoginPage />} />
         <Route path="/admin/inicio" element={<InicioPage />} />
         <Route path="/admin/elecciones" element={<VotacionPage />} />
+        <Route path="/" element={<VotanteLoginPage />} />
+
       </Routes>
     </Router>
   );
